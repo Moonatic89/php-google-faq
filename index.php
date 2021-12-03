5e937e6afc060990aa827965d1f87041d9450229
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-03 16:33:59 +0100

    TEST working on markup/style

--- index.php
+++ index.php
@@ -4,14 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="master.css">
     <title>Document</title>
+<?php include 'header.php'; ?>
+<style>
+<?php include "./assets/css/master.css"; ?>
+</style>
 </head>
 <body>
     
 <header>
-.
-<img src="./img/logo.png" alt="Google Logo">
+
+    
+     <nav>
+         <div class="navTop">
+             <img src="./assets/img/logo.png" alt="Google Logo">
+             <span>
+                 Privacy & Termini
+                </span>
+            </div>
+<div class="navBar botBorder">
+    <ul>
+        <li>Introduzione</li>
+        <li>Norme Sulla Privacy</li>
+        <li>Termini di servizio</li>
+        <li>Tecnologie</li>
+        <li class="blueText">Domande Frequenti</li>
+    </ul>
+</div>
+
+    </nav>
 
 
 
